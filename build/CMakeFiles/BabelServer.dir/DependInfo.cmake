146b86798de419a7bbe4bcf7c9742f70a3eb26f1
cyril <cyril>
2020-09-28 16:26:03 +0200

    start server

---
+++ build/CMakeFiles/BabelServer.dir/DependInfo.cmake
@@ -0,0 +1,79 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cyril/epitech/Ans3/B-CPP-500-MPL-5-1-babel-yoan.vessiere/src/server/main.cpp" "/home/cyril/epitech/Ans3/B-CPP-500-MPL-5-1-babel-yoan.vessiere/build/CMakeFiles/BabelServer.dir/src/server/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG_PROFILE=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/cyril/.conan/data/qt/5.14.2/bincrafters/stable/package/38e652426dd157c4a4aa966ab7cf60c1fd08960f/include"
+  "/home/cyril/.conan/data/portaudio/v190600.20161030/bincrafters/stable/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/opus/1.2.1/bincrafters/stable/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/opus/1.2.1/bincrafters/stable/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include/opus"
+  "/home/cyril/.conan/data/double-conversion/3.1.5/_/_/package/50a5030bbbb13ae56bc4be41915ecd48549cb895/include"
+  "/home/cyril/.conan/data/fontconfig/2.13.91/conan/stable/package/ae28ad6d942ab1a800215a9be1f3c0f4410674e2/include"
+  "/home/cyril/.conan/data/icu/64.2/_/_/package/2339cf7e44ab129a845262fe62027b1d537a2924/include"
+  "/home/cyril/.conan/data/harfbuzz/2.6.4/bincrafters/stable/package/a251f636bfa3fe088f33f369abc54d65544b7ff2/include"
+  "/home/cyril/.conan/data/harfbuzz/2.6.4/bincrafters/stable/package/a251f636bfa3fe088f33f369abc54d65544b7ff2/include/harfbuzz"
+  "/home/cyril/.conan/data/libjpeg/9d/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/sqlite3/3.31.0/_/_/package/01a8a96da981e1edfc87074a67d005b4b5f9950d/include"
+  "/home/cyril/.conan/data/libmysqlclient/8.0.17/_/_/package/c4df4661a2eec2a5c4be03c1632671566d378dac/include"
+  "/home/cyril/.conan/data/libpq/11.5/_/_/package/8e0939db49a1d312829524beb4d0b6824e47691d/include"
+  "/home/cyril/.conan/data/odbc/2.3.7/_/_/package/b29d3eb003873b92a248c0df6debab47f53853ea/include"
+  "/home/cyril/.conan/data/xcb-util-wm/0.4.0/bincrafters/stable/package/8e7869701e9ec44ba722e939fb863671ab14c005/include"
+  "/home/cyril/.conan/data/xcb-util-image/0.4.0/bincrafters/stable/package/fc191a19395e39c93fc4ea02b1f8ad97253bca72/include"
+  "/home/cyril/.conan/data/xcb-util-keysyms/0.4.0/bincrafters/stable/package/8e7869701e9ec44ba722e939fb863671ab14c005/include"
+  "/home/cyril/.conan/data/xcb-util-renderutil/0.3.9/bincrafters/stable/package/8e7869701e9ec44ba722e939fb863671ab14c005/include"
+  "/home/cyril/.conan/data/libxcursor/1.2.0/bincrafters/stable/package/a6b4e97b2b5ecb77554883039f4d0d14c8950e88/include"
+  "/home/cyril/.conan/data/xkbcommon/0.10.0/bincrafters/stable/package/7c01a1c6f3ad359427ed1378838e18ed90776861/include"
+  "/home/cyril/.conan/data/zstd/1.4.4/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/openssl/1.1.1g/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/glib/2.64.0/bincrafters/stable/package/ecb75cbc57a61f14eb7498fe1d80a3fd05c5272b/include"
+  "/home/cyril/.conan/data/glib/2.64.0/bincrafters/stable/package/ecb75cbc57a61f14eb7498fe1d80a3fd05c5272b/include/glib-2.0"
+  "/home/cyril/.conan/data/glib/2.64.0/bincrafters/stable/package/ecb75cbc57a61f14eb7498fe1d80a3fd05c5272b/lib/glib-2.0/include"
+  "/home/cyril/.conan/data/freetype/2.10.1/_/_/package/387eb5152986b9b3cbc2ebb94607d96d90674d67/include"
+  "/home/cyril/.conan/data/freetype/2.10.1/_/_/package/387eb5152986b9b3cbc2ebb94607d96d90674d67/include/freetype2"
+  "/home/cyril/.conan/data/expat/2.2.9/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/libuuid/1.0.3/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/libuuid/1.0.3/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include/uuid"
+  "/home/cyril/.conan/data/libiconv/1.16/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/xcb-util/0.4.0/bincrafters/stable/package/8e7869701e9ec44ba722e939fb863671ab14c005/include"
+  "/home/cyril/.conan/data/libxfixes/5.0.3/bincrafters/stable/package/eda142ac714b42bc2bbc4db9d7b80bca800c4b9b/include"
+  "/home/cyril/.conan/data/libxrender/0.9.10/bincrafters/stable/package/eda142ac714b42bc2bbc4db9d7b80bca800c4b9b/include"
+  "/usr/include/uuid"
+  "/usr/include/freetype2"
+  "/usr/include/libpng16"
+  "/home/cyril/.conan/data/libffi/3.2.1/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/pcre/8.41/_/_/package/3c05ae80ec6af4832a765851eea1c86614091d80/include"
+  "/home/cyril/.conan/data/libelf/0.8.13/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/libmount/2.33.1/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/libmount/2.33.1/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include/libmount"
+  "/home/cyril/.conan/data/libselinux/3.0/_/_/package/6b0384e3aaa343ede5d2bd125e37a0198206de42/include"
+  "/home/cyril/.conan/data/libpng/1.6.37/_/_/package/f99afdbf2a1cc98ba2029817b35103455b6a9b77/include"
+  "/home/cyril/.conan/data/libx11/1.6.8/bincrafters/stable/package/91154d8213bd8cdda5619b42ffdb674c4ab78416/include"
+  "/home/cyril/.conan/data/pcre2/10.33/_/_/package/3b544e62795c293d95ba624360f2fc91ca32ce44/include"
+  "/home/cyril/.conan/data/libxcb/1.13.1/bincrafters/stable/package/9f681d4a18740cb42eb40ba9d0225e9d7e010c5c/include"
+  "/home/cyril/.conan/data/xtrans/1.4.0/bincrafters/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/cyril/.conan/data/zlib/1.2.11/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/include"
+  "/home/cyril/.conan/data/bzip2/1.0.8/_/_/package/da606cf731e334010b0bf6e85a2a6f891b9f36b0/include"
+  "/home/cyril/.conan/data/libxau/1.0.9/bincrafters/stable/package/d3a818d7031243378a00223bdb4035adde9ee8b7/include"
+  "/home/cyril/.conan/data/libxdmcp/1.1.3/bincrafters/stable/package/1fd6dc2a17b4f28db4ca230c3f6252f07288d3d2/include"
+  "/home/cyril/.conan/data/xorgproto/2019.1/bincrafters/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/cyril/.conan/data/xproto/7.0.31/bincrafters/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/BabelServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BabelServer.dir/src/server/main.cpp.o"
+  "bin/BabelServer"
+  "bin/BabelServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BabelServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
