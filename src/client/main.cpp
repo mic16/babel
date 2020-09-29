4af8982be8ecd95158e33ef0eeac0e8afe1ae91b
Timberloft <yoan.vessiere@epitech.eu>
2020-09-29 11:04:16 +0200

    start babel

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -22,26 +22,26 @@ int main(int argc, char **argv)
 	QApplication app (argc, argv);
 	QMainWindow mainWindow;
 	QDesktopWidget dw;
-	QLabel label ("Babel", &mainWindow);
-	QFont font("Arial", 50, QFont::Bold);
-	LineWidget myWidget(QLine(0, 100, 1400, 100), &mainWindow);
-	LineWidget myWidget2(QLine(100, 0, 100, 1400), &mainWindow);
+	// QLabel label ("Babel", &mainWindow);
+	// QFont font("Arial", 50, QFont::Bold);
+	// LineWidget myWidget(QLine(0, 100, 1400, 100), &mainWindow);
+	// LineWidget myWidget2(QLine(100, 0, 100, 1400), &mainWindow);
 
-	mainWindow.setCentralWidget(&myWidget);
+	// mainWindow.setCentralWidget(&myWidget);
 
-	if (app.setStyle("windows") == nullptr)
-		std::cout << "Style error" << std::endl;
+	// if (app.setStyle("windows") == nullptr)
+	// 	std::cout << "Style error" << std::endl;
 
 	int x=dw.width()*0.7;
 	int y=dw.height()*0.7;
 	mainWindow.setFixedSize(x,y);
 	mainWindow.show();
 
-	myWidget.show();
-	myWidget2.show();
+	// myWidget.show();
+	// myWidget2.show();
 
-	label.setFont(font);
-	label.show();
+	// label.setFont(font);
+	// label.show();
 
 	// Pa_Initialize();
     // Pa_Terminate();
