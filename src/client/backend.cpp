54a64f1a3fb246c35af384ff06fb208438673cbf
cyril <cyril>
2020-10-11 16:39:32 +0200

    merge

--- src/client/backend.cpp
+++ src/client/backend.cpp
@@ -440,9 +440,9 @@ void BackEnd::display()
 
 int BackEnd::onAudioReady(const float *inputSamples, unsigned long samplesCount)
 {
-    // for (int i = 0; i < samplesCount - 1; i++) {
-    //     std::cout << i << " : " << inputSamples[i] << std::endl;
-    // }
+    for (int i = 0; i < samplesCount - 1; i++) {
+        std::cout << "onAudioReady : " <<i << " : " << inputSamples[i] << std::endl;
+    }
     callfriend->write(inputSamples, samplesCount);
     return (0);
 }
@@ -450,7 +450,7 @@ int BackEnd::onAudioReady(const float *inputSamples, unsigned long samplesCount)
 int BackEnd::onAudioNeeded(float *outputSamples, unsigned long samplesCount)
 {
     for (int i = 0; i < samplesCount - 1; i++) {
-        std::cout << i << " : " << outputSamples[i] << std::endl;
+        std::cout << "onAudioNeeded : " << i << " : " << outputSamples[i] << std::endl;
     }
     std::memcpy(outputSamples, callfriend->read(samplesCount), samplesCount * sizeof(float));
     return (0);
